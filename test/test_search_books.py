ad48f954ad0fc9759027c785641fe5b5d45d51b6
PatilSac <sachin787521@gmail.com>
2020-06-05 21:56:34 +0530

    Updated xpath for 'Search by Field' and 'Search by Year'

--- test/test_search_books.py
+++ test/test_search_books.py
@@ -4,6 +4,7 @@ import pytest
 import requests
 import xml.etree.ElementTree as ET
 import lxml.html as html
+from decouple import config
 
 
 
@@ -22,6 +23,7 @@ def test_books_dictionary(obj_search, xml_parse):
     assert bool(xml_parse.get_book_name_byfield(text))
 
 
+
 def test_books_byyear_statustest(obj_search):
     code,text = obj_search.search_by_year()
     assert code == 200
@@ -31,28 +33,34 @@ def test_books_byyear_dictionary(obj_search, xml_parse):
     code,text = obj_search.search_by_year()
     assert bool(xml_parse.get_book_name_byyear(text))
 
+
 def test_books_dir_xpath(obj_search):
     root = ET.fromstring(obj_search.search_by_field()[1])
+    author = []
+    title = []
 
-    for node in root.findall('.//best_book/title'):
-        log.info(node.text)
+    for node in root.findall(config('XPATH_TITLE')):
+        # cls.log.info(node.text)
+        title.append(node.text)
 
-    for node in root.findall('.//best_book/author/name'):
-        log.info(node.text)
+    for node in root.findall(config('XPATH_AUTHOR')):
+        # cls.log.info(node.text)
+        author.append(node.text)
 
-    assert True
+    log.info({key: value for (key, value) in zip(author, title)})
 
 def test_books_byyear_xpath(obj_search):
-
     root = html.fromstring(obj_search.search_by_year()[1])
+    author = []
+    title = []
 
-    for node in root.xpath('.//table/tr/td/div/a'):
-        log.info(node)
+    for node in root.xpath(config('XPATH_YEAR_TITLE')):
+        title.append(node.text_content())
 
-    log.info('Done')
-    assert True
+    for node in root.xpath(config('XPATH_YEAR_AUTHOR')):
+        author.append(node.text_content())
 
-
+    log.info({key: value for (key, value) in zip(author, title)})
 
 
 
--- utils/xml_util.py
+++ utils/xml_util.py
@@ -1,7 +1,8 @@
 import os
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 from utils import logging
-
+from decouple import config
+import lxml.html as html
 
 class XMLUtil:
     "Main class for xml based scripts related to this challenge"
@@ -11,6 +12,48 @@ class XMLUtil:
 
     @classmethod
     def get_book_name_byfield(cls,file):
+
+        root = ET.fromstring(file)
+        author = []
+        title = []
+
+        for node in root.findall(config('XPATH_TITLE')):
+            # cls.log.info(node.text)
+            title.append(node.text)
+
+        for node in root.findall(config('XPATH_AUTHOR')):
+            # cls.log.info(node.text)
+            author.append(node.text)
+
+        cls.book = {key: value for (key, value) in zip(author, title)}
+
+        return cls.book
+
+
+    @classmethod
+    def get_book_name_byyear(cls, file):
+
+        author = []
+        title = []
+        root = html.fromstring(file)
+
+        for node in root.xpath(config('XPATH_YEAR_TITLE')):
+            title.append(node.text_content())
+
+        for node in root.xpath(config('XPATH_YEAR_AUTHOR')):
+            author.append(node.text_content())
+
+        cls.book = {key: value for (key, value) in zip(author, title)}
+
+        return cls.book
+
+
+
+
+    """
+        OLD METHOD NEED be commented out or removed, kept for reference.
+    """
+    def _get_book_name_byfield(cls,file):
         #tree = et.ElementTree(file=os.path.dirname(os.getcwd()) + "/ignore/" + "byfield.xml")
         cls.log.info('in get_book method')
 
@@ -18,7 +61,7 @@ class XMLUtil:
             xml_file.write(file)
         cls.log.info('xml file created')
 
-        tree = et.parse(os.path.dirname(os.getcwd()) + "/ignore/" + "byfield.xml")
+        tree = ET.parse(os.path.dirname(os.getcwd()) + "/ignore/" + "byfield.xml")
 
         cls.log.info('tree object created')
 
@@ -38,23 +81,20 @@ class XMLUtil:
 
         return cls.book
 
-    @classmethod
-    def get_book_name_byyear(cls, file):
+    """
+        old method can be commented out or removed, kept for reference.
+    """
+    def _get_book_name_byyear(cls, file):
 
         cls.log.info('in get_book_name_byyear method')
         with open(os.path.dirname(os.getcwd()) + "/ignore/" + "byyear.html", 'w+') as xml_file:
             xml_file.write(file)
         cls.log.info('xml file created')
 
-        tree = et.parse(os.path.dirname(os.getcwd()) + "/ignore/" + "byyear.html")
+        tree = ET.parse(os.path.dirname(os.getcwd()) + "/ignore/" + "byyear.html")
 
         cls.log.info('tree object created')
         root = tree.getroot()
         cls.log.info(list(root))
-
-
-
-
-
 
 
